b787b8a350bcf157b7fdd4e1cda7baad56a6227b
Chethan C K <chethanckm66@gmail.com>
2022-07-14 23:27:55 +0530

    Cinema room manager initial files - Task-2

--- src/com/chethan/Main.java
+++ src/com/chethan/Main.java
@@ -1,22 +1,42 @@
 package com.chethan;
 // https://google.github.io/styleguide/javaguide.html
+
+import java.util.Scanner;
+
 public class Main {
-    static int NO_OF_ROWS = 7;
-    static int NO_OF_COLS = 8;
+
     public static void main(String[] args) {
-        int[][] rooms = new int[NO_OF_ROWS][NO_OF_COLS];
-        System.out.println("Cinema:");
-        System.out.print("  ");
-        for(int i=1; i<=NO_OF_COLS; i++){
-            System.out.print(i+" ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int noOfRows = sc.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int noOfCols = sc.nextInt();
+        int profit;
+        if(noOfRows*noOfCols <= 60){
+            profit = noOfRows*noOfCols*10;
+        } else {
+          if(noOfRows%2 == 0){
+              profit = noOfRows*noOfCols*9;
+          }else{
+              profit = (noOfRows/2)*noOfCols*10;
+              profit += ((noOfRows+1)/2)*noOfCols*8;
+          }
         }
-        System.out.println();
-        for(int i = 1; i <= NO_OF_ROWS; i++){
-            System.out.print(i+" ");
-            for(int j=1; j<=NO_OF_COLS; j++){
-                System.out.print("S ");
-            }
-            System.out.println();
-        }
+        System.out.println("Total income:");
+        System.out.print("$"+profit);
+//        int[][] rooms = new int[noOfRows][noOfCols];
+//        System.out.println("Cinema:");
+//        System.out.print("  ");
+//        for(int i=1; i<=noOfCols; i++){
+//            System.out.print(i+" ");
+//        }
+//        System.out.println();
+//        for(int i = 1; i <= noOfRows; i++){
+//            System.out.print(i+" ");
+//            for(int j=1; j<=noOfCols; j++){
+//                System.out.print("S ");
+//            }
+//            System.out.println();
+//        }
     }
 }
